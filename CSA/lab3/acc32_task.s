5a2ece274421532d080c40d2a15ed3a0f8bc1cda
Gh0st1yAnge1 <yar-shald@yandex.ru>
2026-09-19 22:32:01 +0300

    f32a done

--- CSA/lab3/acc32_task.s
+++ CSA/lab3/acc32_task.s
@@ -8,13 +8,13 @@ temp_b:         .word 0x00
 temp:           .word 0x00
 gcd:            .word 0x01
 
-
     .text
+    .org 0x20
 _start:
     load           input_addr
     load_acc
-    store_addr     a
-    store_addr     temp_a
+    store          a
+    store          temp_a
     
     bgtz           check_b
     load           input_addr
@@ -26,36 +26,35 @@ _start:
 check_b:
     load           input_addr
     load_acc
-    jmp            second_part_of_code
-
-    .org 0x93
-second_part_of_code:
-    store_addr     b
-    store_addr     temp_b
+    store          b
+    store          temp_b
 
     bgtz           find_gcd
     load_imm       -1
-    store_ind       output_addr
+    store_ind      output_addr
     halt
 
 find_gcd:
-    load_addr      temp_b
+    load           temp_b
     beqz           finish_gcd
-    store_addr     temp
-    load_addr      temp_a
+    store          temp
+    load           temp_a
     rem            temp_b
-    store_addr     temp_b
-    load_addr      temp
-    store_addr     temp_a
+    store          temp_b
+    load           temp
+    store          temp_a
     jmp            find_gcd
-    
 
+    jmp            finish_gcd
+
+    .text
+    .org 0x88
 finish_gcd:
-    load_addr      temp_a
-    store_addr     gcd
+    load           temp_a
+    store          gcd
 
 find_lcm:
-    load_addr      a
+    load           a
     div            gcd
     mul            b
     bvs            overflow
